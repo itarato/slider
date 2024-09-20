7b204a6ec723588d32e7bcd729a743f5a7f13259
Peter Arato <it.arato@gmail.com>
2024-09-19 20:33:21 -0400

    Tune materials

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static LevelsController;
 
 public class UIController : MonoBehaviour {
     public TMP_Dropdown packDropdown;
